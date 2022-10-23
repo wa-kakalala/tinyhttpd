844066013da1d5c33e24a8dfa9b6e869e17848be
wangchaosun <2962056732@qq.com>
2022-10-23 16:47:06 +0800

    my tinyhttpd

--- tinyhttpd.c
+++ tinyhttpd.c
@@ -1,276 +1,308 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include <ctype.h>
 #include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<string.h>
 #include <pthread.h>
+#include <sys/stat.h>
 
-#define SERVER_STRING "Server: wkk/0.1.0\r\n"
-typedef unsigned short u16;
-enum METHOD{
-    METHOD_GET,
-    METHOD_POST,
-    METHOD_UNKNOWN
-};
+// if space 
+#define ISspace(x) isspace((int)(x))
+#define SERVER_STRING "Server: wkk's WebServer/0.1\r\n"
 
 /**
-* 输出错误信息，并退出
-* @param  str  错误提示信息
+* print the error info and exit
+* @param sc error info 
 */
-void err_Exit(const char * str){
-    perror(str);
-    exit(1);
+void error(const char *sc){
+ 	perror(sc);
+ 	exit(1);
 }
 
 /**
-* 接收一行数据
-* @param sock 对端socket id 
-* @param buf  缓存区
-* @param size 缓存区大小
-* 返回一行的字符个数
+* create server 's socket fd, and come into listen state
+* @param port spectific port ( if 0,assign by system)
+* @return socket fd and port 
 */
-int get_line(int sock, char *buf, int size)
+int createSockAndListen(u_short *port)
 {
-    int i = 0;
-    char c = '\0';
-    int n = 0;
+	int httpd = 0;
+ 	struct sockaddr_in inaddr;
 
-    while ((i < size - 1) && (c != '\n'))
-    {
-        n = recv(sock, &c, 1, 0);
-        if (n > 0)
-        {
-            if (c == '\r')  // 一行已经结束了， 如果下个字符是\n 要将其读走
-            {
-                // 使用MSG_PEEK 方式查看下一个字符
-                n = recv(sock, &c, 1, MSG_PEEK);
-                if ((n > 0) && (c == '\n'))
-                    recv(sock, &c, 1, 0);
-                else{
-                    c = '\n';
-                }
-            }
-            buf[i++] = c;
-        }else{
-            c = '\n';
-        }
-    }
-    buf[i] = '\0';
-    return i;
+	// use tcp protocol
+ 	httpd = socket(AF_INET, SOCK_STREAM, 0);
+ 	if (httpd == -1) error("[webserver]: create socket failed");
+ 	memset(&inaddr, 0, sizeof(inaddr));
+ 	inaddr.sin_family = AF_INET;
+ 	inaddr.sin_port = htons(*port);
+ 	inaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+ 	//bind address info
+ 	if (bind(httpd, (struct sockaddr *)&inaddr, sizeof(inaddr)) < 0)
+  	error("[webserver]: bind address failed");
+	// dynamically allocating a port 
+ 	if (*port == 0){
+  		socklen_t  addrlen = sizeof(inaddr);
+  		if (getsockname(httpd, (struct sockaddr *)&inaddr, &addrlen) == -1)
+  			error("[webserver]: getsockname failed");
+  		*port = ntohs(inaddr.sin_port);
+	}
+ 	if (listen(httpd, 5) < 0) error("[webserver]: listen failed");
+ 	return httpd;
 }
 
 /**
-* 资源找不到 404 错误
-* @param client  client socket id
+* read one line data: 1. end with \r\n 2. end with \r 3. recv data len < 0
+* @param sock client fd
+* @param buf save line data
+* @param size buf length
+* @return one line data --> buf and data_len
 */
-void not_found_404(int client)
+int getLine(int sock, char *buf, int size)
 {
- char buf[1024];
+ 	int i = 0, n = 0;
+ 	char c = '\0';
 
- sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
- send(client, buf, strlen(buf), 0);
- sprintf(buf, SERVER_STRING);
- send(client, buf, strlen(buf), 0);
- sprintf(buf, "Content-Type: text/html\r\n");
- send(client, buf, strlen(buf), 0);
- sprintf(buf, "\r\n"); //  <---注意点
- send(client, buf, strlen(buf), 0);
- sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
- send(client, buf, strlen(buf), 0);
- sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
- send(client, buf, strlen(buf), 0);
- sprintf(buf, "your request because the resource specified\r\n");
- send(client, buf, strlen(buf), 0);
- sprintf(buf, "is unavailable or nonexistent.\r\n");
- send(client, buf, strlen(buf), 0);
- sprintf(buf, "</BODY></HTML>\r\n");
- send(client, buf, strlen(buf), 0);
+ 	while ((i < size - 1) && (c != '\n')){
+  		n = recv(sock, &c, 1, 0);
+  		if (n > 0){
+   			if (c == '\r'){
+				// look for next char whether is \n
+    			n = recv(sock, &c, 1, MSG_PEEK);
+  				if ((n > 0) && (c == '\n')) // must remove the next char \n
+     				recv(sock, &c, 1, 0);
+    			else  // do not remove the next char
+     				c = '\n';
+   			}
+			buf[i++] = c;
+  		}else
+			c = '\n';
+ 	}
+ 	buf[i] = '\0';
+ 	return i;
 }
 
 /**
-* 处理GET 请求
+* add http headers
+* @param client client fd
 */
-void get_processor(int client){
-    printf("处理 get 请求\r\n");
-    not_found_404(client);
-
+void headers(int client)
+{
+ 	char buf[1024];
+ 	strcpy(buf, "HTTP/1.0 200 OK\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	strcpy(buf, SERVER_STRING);
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "Content-Type: text/html\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	strcpy(buf, "\r\n");
+ 	send(client, buf, strlen(buf), 0);
 }
 
 /**
-* 处理POST 请求
+* the file is unavailable , response 404
 */
-void post_processor(){
-    printf("处理 post 请求\r\n");
-
+void notFindFile(int client){
+ 	char buf[1024];
+ 	sprintf(buf, "HTTP/1.0 404 NOT FOUND\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, SERVER_STRING);
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "Content-Type: text/html\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "\r\n"); // empty line is a must
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "<HTML><TITLE>Not Found</TITLE>\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "<BODY><P>The server could not fulfill\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "your request because the resource specified\r\n");
+	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "is unavailable or nonexistent.\r\n");
+ 	send(client, buf, strlen(buf), 0);
+	sprintf(buf, "</BODY></HTML>\r\n");
+ 	send(client, buf, strlen(buf), 0);
 }
-
-
-
-/* *
- * 处理客户端的连接请求
- * @param arg client socket id
-*/
-void processor(void *arg)
-{
-    int client = (intptr_t)arg;
-    char buf[1024];
-    int numchars;
-    u16 i,j;
-    char methodStr[20];
-    enum METHOD method = METHOD_UNKNOWN;
-    char url[255];
-
-    // 获取第一行信息
-    // "GET / HTTP/1.1\n"
-    // "GET /index.html HTTP/1.1"
-    // buf的最后一位是'\n'(不考虑 '\0')
-    numchars = get_line(client, buf, sizeof(buf));
-    printf("header : %s", buf); 
-    // 提取请求方法
-    i = j = 0;
-    while(buf[i] != ' ' && i < sizeof(method)){
-        methodStr[j++] = buf[i++];
-    }
-    methodStr[j] = '\0';
-    printf("method : %s\r\n", methodStr); 
-
-    if(strcmp(methodStr,"GET") == 0 ) method = METHOD_GET;
-    if(strcmp(methodStr,"POST") == 0 ) method = METHOD_POST;
-    printf("method value : %d \r\n", method);
-
-    // 继续处理后面的信息
-    // 略过空格
-    while(buf[i] != ' ' && i < sizeof(buf)){
-        i++;
-    }
-    // 得到请求地址
-    j = 0;
-    while(buf[i] != ' ' && i < sizeof(buf)){
-        url[j++] = buf[i++];
-    }
-    url[j] = '\0';
-    switch(method){
-        case METHOD_GET:
-            get_processor(client);
-            break;
-        case METHOD_POST:
-            post_processor();
-            break;
-        case METHOD_UNKNOWN:
-            break;
-    }
-
-
-
-
-
-
-
-
-
-
-//  //判断Get请求
-//  if (strcasecmp(method, "GET") == 0)
-//  {
-//   query_string = url;
-//   while ((*query_string != '?') && (*query_string != '\0'))
-//    query_string++;
-//   if (*query_string == '?')
-//   {
-//    cgi = 1;
-//    *query_string = '\0';
-//    query_string++;
-//   }
-//  }
-
-//  //路径
-//  sprintf(path, "htdocs%s", url);
-
-//  //默认地址，解析到的路径如果为/，则自动加上index.html
-//  if (path[strlen(path) - 1] == '/')
-//   strcat(path, "index.html");
-
-//  //获得文件信息
-//  if (stat(path, &st) == -1) {
-//   //把所有http信息读出然后丢弃
-//   while ((numchars > 0) && strcmp("\n", buf))  /* read & discard headers */
-//    numchars = get_line(client, buf, sizeof(buf));
-
-//   //没有找到
-//   not_found(client);
-//  }
-//  else
-//  {
-//   if ((st.st_mode & S_IFMT) == S_IFDIR)
-//    strcat(path, "/index.html");
-//   //如果你的文件默认是有执行权限的，自动解析成cgi程序，如果有执行权限但是不能执行，会接受到报错信号
-//   if ((st.st_mode & S_IXUSR) ||
-//       (st.st_mode & S_IXGRP) ||
-//       (st.st_mode & S_IXOTH)    )
-//    cgi = 1;
-//   if (!cgi)
-//    //接读取文件返回给请求的http客户端
-//    serve_file(client, path);
-//   else
-//    //执行cgi文件
-//    execute_cgi(client, path, method, query_string);
-//  }
-    //执行完毕关闭socket
-    sleep(1);
-    close(client);
-}
-
 
 /**
-* 创建服务器socket 并 监听   
-* @param  port 监听端口
-* 返回 socket_fd
+* HTTP request method not supported
 */
-int start_listen(u16  *port)
-{
-    int socket_fd = 0;
-    struct sockaddr_in servaddr;
+void unImplemented(int client){
+ 	char buf[1024];
 
-    socket_fd = socket(PF_INET,SOCK_STREAM,0);
-    if (socket_fd == -1) err_Exit("socket error");
-  
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(*port);
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    //绑定socket
-    if (bind(socket_fd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
-        err_Exit("bind error");
-    //监听
-    if (listen(socket_fd, 5) < 0)
-        err_Exit("listen error");
-    return socket_fd;
+ 	sprintf(buf, "HTTP/1.0 501 Method Not Implemented\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, SERVER_STRING);
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "Content-Type: text/html\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "<HTML><HEAD><TITLE>Method Not Implemented\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "</TITLE></HEAD>\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "<BODY><P>HTTP request method not supported.\r\n");
+ 	send(client, buf, strlen(buf), 0);
+ 	sprintf(buf, "</BODY></HTML>\r\n");
+ 	send(client, buf, strlen(buf), 0);
 }
 
-int main(void)
+/**
+* get file content and send
+*/
+void cat(int client, FILE *resource){
+ 	char buf[1024];
+	fgets(buf, sizeof(buf), resource);
+ 	while (!feof(resource)){
+  		send(client, buf, strlen(buf), 0);
+  		fgets(buf, sizeof(buf), resource);
+ 	}
+}
+
+void serveFile(int client, const char *filename){
+ 	FILE *resource = NULL;
+ 	int numchars = 1;
+ 	char buf[1024];
+ 	buf[0] = 'A'; buf[1] = '\0';
+	/* read & discard headers */
+ 	while ((numchars > 0) && strcmp("\n", buf))  
+  		numchars = getLine(client, buf, sizeof(buf));
+
+ 	resource = fopen(filename, "r");
+ 	if (resource == NULL)
+  		notFindFile(client);
+	else{
+  		headers(client);
+  		cat(client, resource);
+ 	}
+ 	fclose(resource);
+}
+
+// GET / HTTP/1.1
+// Host: 192.168.0.23:47310
+// Connection: keep-alive
+// Upgrade-Insecure-Requests: 1
+// --------------------------------
+// ------- HTTP POST
+// POST /color1.cgi HTTP/1.1
+// Host: 192.168.0.23 : 47310
+// Connection : keep-alive
+// Content-Length : 10
+
+/**
+* deal with client 's request  --> just support GET now 
+* @param arg client fd
+*/
+void acceptRequest(void *arg)
 {
-    int server_sock = -1;
-    u16 port = 8000;
-    int client_sock = -1;
+  	//get client socket fd from ptr struct
+ 	int client = (intptr_t)arg;
+ 	int numchars;
+	/*
+	 method : request method GET ? POST
+	 url : request url 
+	path : request file path
+	*/
+	char buf[1024],method[20],url[255],path[512];
 
-    pthread_t newthread;
+ 	unsigned int i = 0, j = 0;
+ 	struct stat st;   
+ 	char *query_string = NULL;
 
-    server_sock = start_listen(&port);
-    printf("tiny http running on port %d\n", port);
+ 	//GET / HTTP/1.1\n
+ 	numchars = getLine(client, buf, sizeof(buf));
+	//extract GET / POST	
+ 	while (!ISspace(buf[j]) && (i < sizeof(method) - 1))
+  		method[i++] = buf[j++];
+ 	method[i] = '\0';
 
-    while (1)
-    {  
-        client_sock = accept(server_sock,(struct sockaddr*)NULL,NULL);
-        if (client_sock == -1)
-	        err_Exit("accept error");
-        printf("接入 client\r\n");
+ 	//judge GET or POST --> both not to response umImplemented
+ 	if (strcasecmp(method, "GET") && strcasecmp(method, "POST")){
+  		unImplemented(client);
+  		return;
+ 	}
+ 	i = 0;
+ 	//skip space
+ 	while (ISspace(buf[j]) && (j < sizeof(buf))) j++;
 
-        // 创建线程
-        if (pthread_create(&newthread, NULL, (void *)processor, (void *)(intptr_t)client_sock) != 0)
-	       perror("pthread_create");
-    }
-    close(server_sock);
-    return 0;
+    //url : http://192.168.0.23:47310/index.html
+ 	//http info : GET /index.html HTTP/1.1，
+ 	while (!ISspace(buf[j]) && (i < sizeof(url) - 1) && (j < sizeof(buf)))
+  		url[i++] = buf[j++];
+	url[i] = '\0';  // for example : /index.html
+
+	//POST , Not currently support
+ 	if (strcasecmp(method, "POST") == 0)
+  		return ; 
+ 	//GET
+ 	if (strcasecmp(method, "GET") == 0){
+  		query_string = url;
+		// find ? 
+  		while ((*query_string != '?') && (*query_string != '\0'))
+   			query_string++;
+  		if (*query_string == '?'){
+			// url content between / and ?
+   			*query_string = '\0';
+			// after ? is query_string
+   			query_string++;
+  		}
+ 	}
+ 	//get file path
+ 	sprintf(path, "data%s", url);
+ 	//whether path is /,add index.html
+ 	if (path[strlen(path) - 1] == '/')
+  		strcat(path, "index.html");
+
+ 	//get file info 
+ 	if (stat(path, &st) == -1) {
+		// get all the http info ,and then discard
+  		while ((numchars > 0) && strcmp("\n", buf)) 
+   			numchars = getLine(client, buf, sizeof(buf));
+  		notFindFile(client);
+		return ;
+ 	}else{
+		// is directory
+  		if ((st.st_mode & S_IFMT) == S_IFDIR)
+   			strcat(path, "/index.html");
+
+  		if (!(st.st_mode & S_IRUSR) &&
+      		!(st.st_mode & S_IRGRP) &&
+      		!(st.st_mode & S_IROTH)){
+			printf("[WebServer]: no permission to read the file\r\n");
+		}
+		serveFile(client, path);
+ 	}
+ 	close(client);
+}
+
+/**
+* @param argv can give one parameter --> port 
+*/
+int main(int argc ,char * argv[]){
+	int server_sock = -1,client_sock = -1;
+	u_short port = (argc == 2)?atoi(argv[1]):0;
+	struct sockaddr_in client_name;
+ 	socklen_t client_name_len = sizeof(client_name);
+	// create thread variable
+ 	pthread_t newthread;
+ 	server_sock = createSockAndListen(&port);
+ 	printf("[webserver]: WebServer running on port %d\r\n", port);
+
+ 	while (1){
+	 	client_sock = accept(server_sock,
+                       (struct sockaddr *)&client_name,
+                       &client_name_len);
+  		if (client_sock == -1)	error("[webserver]: accept failed");
+		//create thread to deal with new request
+		//convert client fd to address parameter ( intptr_t is safe )
+  		if (pthread_create(&newthread, NULL,(void* (*)(void*))acceptRequest,
+				 (void *)(intptr_t)client_sock) != 0)
+	  		error("pthread_create failed");
+ 	}
+	close(server_sock);
+	return 0;
 }
